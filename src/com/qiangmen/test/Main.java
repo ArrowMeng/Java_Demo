030b39096409d08108d73e7f7a56df184458ce73
Arrow Meng <mqj_hzau@aliyun.com>
2016-11-26 20:07:44 +0800

    test tag

--- src/com/qiangmen/test/Main.java
+++ src/com/qiangmen/test/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(System.getenv());
         System.out.println(System.getenv("USER"));
+
+        System.out.println(Integer.getInteger("VERSION", 1));
     }
 }

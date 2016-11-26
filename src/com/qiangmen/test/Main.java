4297398e869d1efd27cfa707d2f67ede6179d139
Arrow Meng <mqj_hzau@aliyun.com>
2016-11-26 20:00:16 +0800

    FirstClass

---
+++ src/com/qiangmen/test/Main.java
@@ -0,0 +1,11 @@
+package com.qiangmen.test;
+
+/**
+ * Created by qiangmen on 11/26/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(System.getenv());
+        System.out.println(System.getenv("USER"));
+    }
+}
